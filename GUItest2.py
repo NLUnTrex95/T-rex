1b5fa3e0ae31584934d5c8a58ea8c5fd07510858
NLUnTrex95 <63023683+NLUnTrex95@users.noreply.github.com>
2020-04-02 01:36:15 -0500

    Update GUItest2.py

--- GUItest2.py
+++ GUItest2.py
@@ -104,7 +104,7 @@ def checker(test_dict):
         else:
             c = value
         if isinstance(c, int) == False:
-            print(c, "Bad!")
+            print(c, "Bad!") #this will be an error popup.
 
 def converter(test_dict):
     i = 0
@@ -118,6 +118,8 @@ def converter(test_dict):
         convlist[i] = test_dict[stats_tracking_list[i]]
         i += 1
     return convlist
+## Possible include in class
+
 
 def MonthDayNumbers(month):
     thirtyone = [1, 3, 5, 7, 8, 10, 12]
@@ -130,32 +132,38 @@ def MonthDayNumbers(month):
         output = 28
     return output
 
+  
 def FractionalYears():
+  
+  
     def append():
         yearsout.append(round((deplist[2] + 1), 2))
         nbvout.append(round(deplist[1], 2))
         adout.append(round((deplist[0] - deplist[1]), 2))
         if deplist[5] == "SOYD":
-            depout.append(round(((1 / ((deplist[2] * (deplist[2] + 1)) / 2)) * deplist[0]),2))
+            depout.append(round(((1 / ((deplist[2] * (deplist[2] + 1)) / 2)) * deplist[0]),2))  
     yearmult = (13 - deplist[3])
     if deplist[4] != 1:
-        yearmult += round((MonthDayNumbers(deplist[3]) - deplist[4]) / MonthDayNumbers(deplist[3]),2)
+        yearmult -= round((MonthDayNumbers(deplist[3]) - deplist[4]) / MonthDayNumbers(deplist[3]),2) ##Check whether it's += or -=...?
     yearmult /= 12
     yearmult = round(yearmult, 2)
     if deplist[5] == "Straight Line" or deplist[5] == "SOYD":
         append()
     return yearmult
 
-def LastYear(s):
+  
+def LastYear(s):  ## Possible class usage.
     yearsout[s] += 1
     yearsout[s] += yearsout[s - 1]
     nbvout[s] += nbvout[s - 1]
     adout[s] += adout[s - 1]
 
+## Possible class usage, used in every method.    
 def Increment(s):
     nbvout[s] -= round(depout[s],2)
     adout[s] += round(depout[s],2)
 
+## Possible class distinction    
 def stop(s):
     adout[s] = deplist[0] - deplist[1]
     depout[s] = adout[s]-adout[s-1]
@@ -163,6 +171,8 @@ def stop(s):
     for y in range(s+1, deplist[2]):
         yearsout[y] += 1
         yearsout[y] += yearsout[y - 1]
+        
+## Possible class distinction
 def astop(s):
     depout[s] = deplist[1] - nbvout[s]
     nbvout[s] = deplist[1]
@@ -172,12 +182,15 @@ def astop(s):
 deplist = converter(test_dict)  #Line 119
 checker(test_dict)  #Line 109
 yearsp = deplist[2]
-yearsout = [0] * yearsp
+## yearsout = depout = nbvout = adout = ([0] * deplist[2])
+yearsout = [0] * yearsp #Possible for loop spot.
 depout = [0] * yearsp
 nbvout = [0] * yearsp
 nbvout[0] = deplist[0]  #Setting the initial NBV to the Cost.
 adout = [0] * yearsp
+## may need relocation.
 activity = [100, 200, 300, 250, 150]  #Only for activity basis. Filler values in lieu of user inputs
+
 
 def StraightLine():
     depbase = deplist[0] - deplist[1]
@@ -188,16 +201,16 @@ def StraightLine():
             depout[s] *= FractionalYears()
             depout.append(round((depbase / deplist[2]) - depout[0], 2))
         Increment(s)
-    output = {"Year": yearsout, "Depreciation": depout, "NBV": nbvout, "A/D": adout}
+    output = {"Year": yearsout, "Depreciation": depout, "NBV": nbvout, "A/D": adout,}
     return output
 
 
 def DoubleDeclining():
-    for s in range(0, deplist[2]):
+    for s in range(0, deplist[2]): ## class posibly. In all formats.
         LastYear(s)
+        depout[s] = nbvout[s] * (2 / deplist[2]) ##Moved this to below the previous phrase. We can't multiply nothing ;D
         if s == 0 and (deplist[3] != 1 or deplist[4] != 1):
             depout[s] *= FractionalYears()
-        depout[s] = nbvout[s] * (2 / deplist[2])
         Increment(s)
         if nbvout[s] < deplist[1]:
             stop(s)
@@ -205,6 +218,7 @@ def DoubleDeclining():
     output = {"Year": yearsout, "Depreciation": depout, "NBV": nbvout, "A/D": adout}
     return output
 
+  
 def SumOfYears():
     for s in range(0, deplist[2]):
         LastYear(s)
@@ -220,6 +234,7 @@ def SumOfYears():
     output = {"Year": yearsout, "Depreciation": depout, "NBV": nbvout, "A/D": adout}
     return output
 
+  
 def ActivityBased():
     albase = 1000 #Temporary variable in lieu of user input.
     ucost = (deplist[0] - deplist[1]) / albase
@@ -234,26 +249,29 @@ def ActivityBased():
     output = {"Year": yearsout, "Depreciation": depout, "NBV": nbvout, "A/D": adout}
     return output
 
+  
 def MACRS():
     MACRS = [.2, .32, .192, .1152, .1152, .0576, .1429, .2449, .1749, .1249, .0893, .0892, .0893, .0446,  ##Half Year
-             .35, .26, .156, .1101, .1101, .0138, .25, .2143, .1531, .1093, .0875, .0874, .0875, .0109,  ##MQ, Q1
-             .25, .3, .18, .1137, .1137, .0426, .1785, .2347, .1676, .1197, .0887, .0887, .0887, .0334,  ##MQ, Q2
-             .15, .34, .204, .1224, .1130, .0706, .1071, .2551, .1822, .1302, .0930, .0885, .0886, .0553,  ##MQ, Q3
-             .05, .038, .228, .1368, .1094, .0958, .0357, .2755, .1968, .1406, .1004, .0873, .0873, .0764]  ##MQ, Q4
-    conv = 1 #Convention auto-filled in lieu of user input
-    m = 0 if deplist[2] == 5 else 6 
-	quart = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
-	MList = [[20,34,48,62],[14,28,42,56]]
-	if deplist[2] == 5:
-  		for x in range(0,3)
-    		if deplist[2] in quart[x]:
-      		m = MList[0[x]]
-	elif deplist[2] == 7:
-  		for x in range(0,3)
-    	if deplist[2] in quart[x]:
-      		m = MList[1[x]]
-	else:
-  		root.mainloop
+               .35, .26, .156, .1101, .1101, .0138, .25, .2143, .1531, .1093, .0875, .0874, .0875, .0109,  ##MQ, Q1
+               .25, .3, .18, .1137, .1137, .0426, .1785, .2347, .1676, .1197, .0887, .0887, .0887, .0334,  ##MQ, Q2
+               .15, .34, .204, .1224, .1130, .0706, .1071, .2551, .1822, .1302, .0930, .0885, .0886, .0553,  ##MQ, Q3
+               .05, .038, .228, .1368, .1094, .0958, .0357, .2755, .1968, .1406, .1004, .0873, .0873, .0764]  ##MQ, Q4
+    conv = "Half Year" #Convention auto-filled in lieu of user input.
+    quart = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
+    MList = [[20,34,48,62],[14,28,42,56]]
+    if conv == "Half Year":
+    	m = 0 if deplist[2] == 5 else 6 
+    elif conv == "Mid Quarter":
+      if deplist[2] == 5:
+        for x in range(0,3)
+          if deplist[2] in quart[x]:
+            m = MList[0[x]]
+      elif deplist[2] == 7:
+          for x in range(0,3)
+          if deplist[2] in quart[x]:
+              m = MList[1[x]]
+    else:
+        root.mainloop
     for s in range(0, deplist[2]):
         LastYear(s)
         depout[s] = MACRS[m] * deplist[0]
@@ -262,7 +280,7 @@ def MACRS():
     output = {"Year": yearsout, "Depreciation": depout, "NBV": nbvout, "A/D": adout}
     return output
 
-running = True
+running = True ## Relocate this.
 ##The Main Program Area
 while running:
     if deplist[5] == "Straight Line":
@@ -275,5 +293,9 @@ while running:
         backout = ActivityBased()
     elif deplist[5] == "MACRS":
         backout = MACRS()
-    print(backout)
+    print(backout) ## this will post to new screen. Table.
     end = input("Press Enter")
+'''
+This is a sample backout list.
+{"Year": [1,2,3,4,5], "Depreciation": [20000,20000,20000,20000,20000], "NBV": [100000,80000,60000,40000,20000], "A/D": [20000,40000,60000,80000,100000]}    
+'''
